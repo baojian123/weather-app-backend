54848d311f74e036755184f859a247d5a59f6563
Zhian Chen <351211168@qq.com>
2021-02-02 18:15:07 +1100

    Add feature of current weather query

--- src/controllers/api/v1/fakedata.js
+++ src/controllers/api/v1/fakedata.js
@@ -1,12 +1,24 @@
 
-exports.geoinfo = async (ctx) => {
+exports.iplookup = async (ctx) => {
   const location = require('../assets/location.json');
   console.log("from fake data");
   ctx.body = location
+}
+
+exports.currentWeather = async (ctx) => {
+  const current = require('../assets/current.json');
+  console.log("from fake data")
+  ctx.body = current
 }
 
 exports.fortnightWeather = async (ctx) => {
   const fortnight = require('../assets/fortnight.json');
   console.log("from fake data")
   ctx.body = fortnight
+}
+
+exports.hourlyWeather = async (ctx) => {
+  const hourly = require('../assets/hourly.json');
+  console.log("from fake data")
+  ctx.body = hourly
 }
--- src/controllers/api/v1/weather.js
+++ src/controllers/api/v1/weather.js
@@ -1,8 +1,8 @@
 
-exports.geoinfo = async (ctx) => {
+exports.iplookup = async (ctx) => {
+  const api = `https://weatherapi-com.p.rapidapi.com/ip.json?`
   let geoLocation
   let {request:{body:{ip}}} = ctx
-  let api = `https://weatherapi-com.p.rapidapi.com/ip.json?`
   let headers = {
     'x-rapidapi-key': '024879661bmshc2dde30467e740bp1a770cjsn1eba8decc6c4',
     'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
@@ -15,17 +15,51 @@ exports.geoinfo = async (ctx) => {
   }
 }
 
-exports.fortnightWeather = async (ctx) => {
+exports.currentWeather = async (ctx) => {
   let {lat, lon} = ctx.request.body
   let weatherInfo
-  let api = "https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily"
+  const api ='https://weatherbit-v1-mashape.p.rapidapi.com/current'
   let headers = {
-    "x-rapidapi-key": "024879661bmshc2dde30467e740bp1a770cjsn1eba8decc6c4",
-  	"x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com",
-  	"useQueryString": true
+    'x-rapidapi-key': '024879661bmshc2dde30467e740bp1a770cjsn1eba8decc6c4',
+  	'x-rapidapi-host': 'weatherbit-v1-mashape.p.rapidapi.com',
+  	'useQueryString': true
   }
   try{
     weatherInfo = await ctx.get(api, {lat:lat, lon:lon}, headers)
+    ctx.body = weatherInfo
+  }catch(e) {
+    console.log(e)
+  }
+}
+
+exports.fortnightWeather = async (ctx) => {
+  let {lat, lon} = ctx.request.body
+  let weatherInfo
+  const api = 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily'
+  let headers = {
+    'x-rapidapi-key': '024879661bmshc2dde30467e740bp1a770cjsn1eba8decc6c4',
+  	'x-rapidapi-host': 'weatherbit-v1-mashape.p.rapidapi.com',
+  	'useQueryString': true
+  }
+  try{
+    weatherInfo = await ctx.get(api, {lat:lat, lon:lon}, headers)
+    ctx.body = weatherInfo
+  }catch(e) {
+    console.log(e)
+  }
+}
+
+exports.hourlyWeather = async (ctx) => {
+  let {lat, lon} = ctx.request.body
+  let weatherInfo
+  const api = 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/hourly'
+  let headers = {
+    'x-rapidapi-key': '024879661bmshc2dde30467e740bp1a770cjsn1eba8decc6c4',
+  	'x-rapidapi-host': 'weatherbit-v1-mashape.p.rapidapi.com',
+  	'useQueryString': true
+  }
+  try{
+    weatherInfo = await ctx.get(api, {lat: lat, lon: lon}, headers)
     ctx.body = weatherInfo
   }catch(e) {
     console.log(e)
